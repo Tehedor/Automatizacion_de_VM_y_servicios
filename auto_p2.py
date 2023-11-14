4b2747a5c0c2b171c46151bf4cbe80c87a7e8915
Sergio Tejedor <sergiot1805@gmail.com>
2023-11-14 19:35:48 +0100

    automatización de auto

--- auto_p2.py
+++ auto_p2.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python
-
+from subprocess import call
 from lib_mv import MV,Red
 import logging, sys, json
 
+# #########################################################################
 
+with open('auto_p2.json', 'r') as f:
+    data = json.load(f)
+
+
+num_server = data['num_server']
+# debug = data['debug']
+
+if len(sys.argv) < 2:
+    print("Error: No se proporcionó el segundo argumento.")
+    sys.exit(1)
+
+second_arg = sys.argv[1]
+
+next_arg = []
+i=2
+
+all_vm = ["c1","lb","s1","s2","s3"] 
+if len(sys.argv) > 1:
+    if sys.argv[2] == " ":
+        next_arg = all_vm
+
+    else:
+        while i < len(sys.argv) and sys.argv[i] != " ":
+            if sys.argv[i] != "lb" and sys.argv[i] != "c1" and (self.name.startswith("s") and self.name[1:].isdigit()):
+                next_arg.append(sys.argv[i])
+                i = i + 1
+            else:
+                print(f"maquina {sys.argv[i]} no se puede crear")
 
 # #########################################################################
 def init_log():
@@ -23,117 +52,131 @@ def pause():
 init_log()
 print('CDPS - mensaje info1')
 # #########################################################################
+# #########################################################################
+
+def crear_fiche(self,ip,router):
+
+    with open ('interfaces','w') as archivo:
+    if router == True:
+      archivo.write("auto lo\n")
+      archivo.write("iface lo inet loopback\n\n")
+      archivo.write("auto eth0\n")
+      archivo.write("iface eth0 inet static\n")
+      archivo.write(f"\taddress {ip[0]}\n")
+      archivo.write("\tnetmask 255.255.255.0\n")
+      archivo.write("auto eth1\n")
+      archivo.write("iface eth1 inet static\n")
+      archivo.write(f"\taddress {ip[1]}\n")
+      archivo.write("\tnetmask 255.255.255.0\n")
+    else:
+      if nombre.startswith("s"):
+        archivo.write("auto lo\n")
+        archivo.write("iface lo inet loopback\n\n")
+        archivo.write("auto eth0\n")
+        archivo.write("iface eth0 inet static\n")
+        archivo.write(f"\taddress {ip[0]}\n")
+        archivo.write("\tnetmask 255.255.255.0\n")
+        archivo.write("\tgateway 10.11.2.1\n")
+      else:
+        archivo.write("auto lo\n")
+        archivo.write("iface lo inet loopback\n\n")
+        archivo.write("auto eth0\n")
+        archivo.write("iface eth0 inet static\n")
+        archivo.write(f"\taddress {ip[0]}\n")
+        archivo.write("\tnetmask 255.255.255.0\n")
+        archivo.write("\tgateway 10.11.1.1\n")
+  
+    with open ('hostname','w') as archivo:
+        archivo.write(self.nombre)
 
 
-with open('auto_p2.json', 'r') as f:
-    data = json.load(f)
+    call(["sudo","virt-copy-in", "-a", self.nombre + ".qcow2", "hostname", "/etc/"])
+    call(["rm","hostname"])
+    call(["sudo","virt-copy-in", "-a", self.nombre + ".qcow2", "interfaces", "/etc/network/"])
+    call(["rm","interfaces"])
+    call(["sudo", "virt-edit", "-a", self.nombre + ".qcow2", "/etc/hosts", "-e", f"s/127.0.1.1.*/127.0.1.1 {nombre}/"])
 
+# #########################################################################
 
-num_server = data['num_server']
-# debug = data['debug']
+def ip_control(self):
+    if self.name == "c1"
+        ip = ["10.11.1.2"]
+    elif self.name == "lb"
+        ip = ["10.11.1.1","10.11.2.1"]
+    elif self.name.startswith("s") and self.name[1:].isdigit():
+        ip = ["10.11.2.3"+self.name[1:]]
+    return ip
 
-if len(sys.argv) < 2:
-    print("Error: No se proporcionó el segundo argumento.")
-    sys.exit(1)
+def interfaces_control(self):
+    if self.name == "c1"
+        interface = ["LAN1"]
+    elif self.name == "lb"
+        interface = ["LAN1","LAN2"]
+    elif self.name.startswith("s") and self.name[1:].isdigit():
+        interface = ["LAN2"]
+    return interface
 
-second_arg = sys.argv[1]
-
+# def interfaces_control(self):
+#     if self.name == "c1"
+#         interface = ["if1"]
+#     elif self.name == "lb"
+#         interface = ["if1","if2"]
+#     elif self.name.startswith("s") and self.name[1:].isdigit():
+#         interface = ["if2"]
+#     return interface
 
 
 
 if second_arg == 'crear':
     imagen = "cdps-vm-base-pc1.qcow2"   
-    if1 = Red("LAN1")
-    if2 = Red("LAN2")
-    if1.crear_red()
-    if2.crear_red()
     
-    # def crear_mv (self, imagen, interfaces_red, router):
-    # Creacion de rotuer
-    lb = MV("LB")
-    lb.crear_mv( imagen,['if1', 'if2'], True)
-    ip_lb = ["10.11.1.1","10.11.2.1"]
-    crear_fiche(lb,ip_lb,True)
-    # Creacion de clinete
-    c1 = MV("c1")
-    c1.crear_mv(imagen, ['if1'], False)
-    ip_c1 = ["10.11.1.2"]
-    crear_fiche(c1,ip_c1,False)
-    # Creación de Servidores
-    num = "31"
-    for num_server in range(1,num_server+1):
-        name = "s" + num_server
-        mv.crear_mv(imagen, ['if1'] , False)
-        ip_mv = [f"10.11.2.{num}"]
-        crear_fiche(mv,ip_mv,False)
-        num = num + 1
+    if next_arg == all_vm:
+        if1 = Red("LAN1")
+        if2 = Red("LAN2")
+        if1.crear_red()
+        if2.crear_red()
+    
+    for nombre_mv in next_arg:
+        nombre = MV(nombre_mv)
+        interface_red = interfaces_control(nombre)
+        ip_red = ip_control(nombre)
+        router = False
+        if nombre_mv == "lb":
+            router = True
+        nombre.crear_mv(imagen,interface_red, router)
+        crear_fiche(nombre,ip_red,router)
+    
 elif second_arg == 'arrancar':
     # Arrancar maquinas
-    lb.arrancar_mv()
-    c1.arrancar_mv()
-    for num_server in range(1,num_server+1):
-        name = "s" + num_server
-        name.arrancar_mv()  
-    # Arrancar redes
-    if1.arrancar_red()
-    if2.arrancar_red()
 
+
+    for nombre_mv in next_arg:
+        nombre = MV(nombre_mv)
+        nombre.arrancar_mv() 
+    
+    # Arrancar redes
+    if next_arg == all_vm:
+        if1 = Red("LAN1")
+        if2 = Red("LAN2")
+        if1.arrancar_red()
+        if2.arrancar_red()
 
 
 elif second_arg == 'parar':
-    lb.parar_mv()
-    c1.parar_mv()
-    for num_server in range(1,num_server+1):
-        name = "s" + num_server
-        name.parar_mv()  
+    for nombre_mv in next_arg:
+        nombre = MV(nombre_mv)
+        nombre.parar_mv()
+      
 elif second_arg == 'liberar':
-    lb.liberar_red()
-    c1.liberar_red()   
-    for num_server in range(1,num_server+1):
-        name = "s" + num_server
-        name.liberar_red()  
+    for nombre_mv in next_arg:
+        nombre = MV(nombre_mv)
+        nombre.liberar_mv()
+   
     # Liberar redes
-    if1.liberar_red()
-    if2.liberar_red()
+    if next_arg == all_vm:
+        if1 = Red("LAN1")
+        if2 = Red("LAN2")
+        if1.liberar_red()
+        if2.liberar_red()
 else:
     print(f"Error: Argumento desconocido {second_arg}")
-
-def crear_fiche(self,ip,router):
-    with open ('interfaces','w') as archivo:
-        
-        if router == True:
-            archivo.write("auto lo\n")
-            archivo.write("iface lo inet loopback\n\n")
-            archivo.write("auto eth0\n")
-            archivo.write("iface eth0 inet static\n")
-            archivo.write(f"\taddress {ip[0]}\n")
-            archivo.write("\tnetmask 255.255.255.0\n")
-            archivo.write("\tgateway 10.11.2.33\n")
-            archivo.write("auto eth1\n")
-            archivo.write("iface eth1 inet static\n")
-            archivo.write(f"\taddress {ip[1]}\n")
-            archivo.write("\tnetmask 255.255.255.0\n")
-            archivo.write("\tgateway 10.11.2.33\n")
-        else:
-            if nombre.startswith("s"):
-                archivo.write("auto lo\n")
-                archivo.write("iface lo inet loopback\n\n")
-                archivo.write("auto eth1\n")
-                archivo.write("iface eth1 inet static\n")
-                archivo.write(f"\taddress {ip[0]}\n")
-                archivo.write("\tnetmask 255.255.255.0\n")
-                archivo.write("\tgateway 10.11.2.1\n")
-            else:
-                archivo.write("auto lo\n")
-                archivo.write("iface lo inet loopback\n\n")
-                archivo.write("auto eth0\n")
-                archivo.write("iface eth0 inet static\n")
-                archivo.write(f"\taddress {ip[0]}\n")
-                archivo.write("\tnetmask 255.255.255.0\n")
-                archivo.write("\tgateway 10.11.1.1\n")
-                
-
-    call(["sudo","virth-copy-in", "-a", self.nombre + ".qcow2", "hostname", "/etc/"])
-    call(["sudo","virth-copy-in", "-a", self.nombre + ".qcow2", "interfaces", "/etc/network/"])
-    call(["sudo","virth-edit", "-a", self.nombre + ".qcow2", "/etc/hosts", "-e","/'127.0.1.1.*/127.0.1.1" + " " + self.nombre + "'/"])
-    
