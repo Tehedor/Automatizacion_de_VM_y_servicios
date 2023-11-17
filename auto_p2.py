ea804cfd5bd20e7006557ec779f6308474aaf003
Sergio Tejedor <sergiot1805@gmail.com>
2023-11-17 15:06:28 +0100

    Añadido el control de maquinas por medio del cotrol file

--- auto_p2.py
+++ auto_p2.py
@@ -135,7 +135,8 @@ def control_state(nombre, state):
                         return False
     return False
 
-def control_state_mac(nombre, state):
+# def control_state_mac(nombre, state):
+def control_change_state(nombre, state):
     call(["cp","control_file","control_file_copia"])
 
     exite = control_search(nombre)
@@ -145,7 +146,7 @@ def control_state_mac(nombre, state):
     file = open("control_file","w") 
 
    
-    cambios = False    
+    # cambios = False    
     for line in copia:
         c = 0
         if exite == True:
@@ -156,7 +157,7 @@ def control_state_mac(nombre, state):
                     line = "\t" + palabras[0] + palabras[1] + "\n"
                     file.write(line)
                     c = 1
-                    cambios = True
+                    # cambios = True
         if c == 0: 
             file.write(line)
                 # return True
@@ -165,7 +166,7 @@ def control_state_mac(nombre, state):
     copia.close()
     call(["rm","control_file_copia"])
     # return True
-    return cambios
+    # return cambios
 
 
 # #########################################################################
@@ -183,25 +184,21 @@ all_vm = ["c1","lb"]
 for i in range(num_server):
     all_vm.append("s" + str(i+1)) 
 
-i = 2
+
 if len(sys.argv) > 1:
     if sys.argv[2] == " ":
         next_arg = all_vm
     else:
-        while i < len(sys.argv) and sys .argv[i] != " ":
+        while i < len(sys.argv) and sys.argv[i] != " ":
             if sys.argv[i] != "lb" and sys.argv[i] != "c1" and (self.name.startswith("s") and self.name[1:].isdigit()):
-                print(f"maquina {sys.argv[i]} no se puede crear")                
+                print(f"maquina {sys.argv[i]} no permitida")                
             else:
-                while i < len(sys.argv) and sys .argv[i] != " ":
-                    if sys.argv[i] != "lb" and sys.argv[i] != "c1" and (self.name.startswith("s") and self.name[1:].isdigit()):
-                        print(f"maquina {sys.argv[i]} no se puede crear")
-                    else:
-                        if control_search(sys.argv[i]) == True:
-                            print(f"maquina {sys.argv[i]} ya existe")
-                        else:
-                            next_arg.append(sys.argv[i])
-                        i = i + 1
+                next_arg.append(sys.argv[i])
+            i = i + 1
 
+# #########################################################################
+# #########################################################################
+# Ejecucion
 # #########################################################################
 # #########################################################################
 
@@ -209,52 +206,68 @@ if len(sys.argv) > 1:
 if second_arg == 'crear':
     imagen = "cdps-vm-base-pc1.qcow2"   
     
-    if next_arg == all_vm:
+    if not control_state("LAN","1"):
         if1 = Red("LAN1")
         if2 = Red("LAN2")
         if1.crear_red()
         if2.crear_red()
+        control_add("LAN")
     
     for nombre_mv in next_arg:
-        nombre = MV(nombre_mv)
-        router = False
-        if nombre_mv == "lb":
-            router = True
-        nombre.crear_mv(imagen,interface_red, router)
+        if not control_search(nombre_mv):
+            nombre = MV(nombre_mv)
+            router = False
+            if nombre_mv == "lb":
+                router = True
+            nombre.crear_mv(imagen,interface_red, router)
+            control_add(nombre_mv)
+        else:
+            print(f"Error: La maquina {nombre_mv} ya existe")
     
 elif second_arg == 'arrancar':
+
     # Arrancar maquinas
-
-
     for nombre_mv in next_arg:
-        nombre = MV(nombre_mv)
-        nombre.arrancar_mv() 
-        nombre.mostrar_consola_mv()
-
-    # Arrancar redes
-    if next_arg == all_vm:
-        if1 = Red("LAN1")
-        if2 = Red("LAN2")
-        if1.arrancar_red()
-        if2.arrancar_red()
+        if control_search(nombre_mv):
+            if control_state(nombre_mv,"0"):    
+                nombre = MV(nombre_mv)
+                nombre.arrancar_mv() 
+                nombre.mostrar_consola_mv()
+                control_change_state(nombre_mv,"1")
+            else:
+                print(f"Error: La maquina {nombre_mv} ya esta arrancada")
+        else:
+            print(f"Error: La maquina {nombre_mv} no existe")
 
 
 elif second_arg == 'parar':
     for nombre_mv in next_arg:
-        nombre = MV(nombre_mv)
-        nombre.parar_mv()
+        if control_search(nombre_mv):
+            if control_state(nombre_mv,"1"):
+                nombre = MV(nombre_mv)
+                nombre.parar_mv()
+                control_change_state(nombre_mv,"0")
+            else:
+                print(f"Error: La maquina {nombre_mv} ya esta parada")
+        else:
+            print(f"Error: La maquina {nombre_mv} no existe")
       
 elif second_arg == 'liberar':
     for nombre_mv in next_arg:
         nombre = MV(nombre_mv)
         nombre.liberar_mv()
+        control_rm(nombre_mv)
    
     # Liberar redes
-    if next_arg == all_vm:
+    with open ('control_file','r') as archivo:
+        n_lines = len(archivo.readlines())
+
+    if n_lines < 5:
         if1 = Red("LAN1")
         if2 = Red("LAN2")
         if1.liberar_red()
         if2.liberar_red()
+        control_state_mac("LAN","0")
 
 elif second_arg == 'consola':
     for nombre_mv in next_arg:
