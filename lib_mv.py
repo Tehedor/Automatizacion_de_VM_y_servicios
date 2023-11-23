0a3468acd827cb12ba5e411d31522aef9d1f5e89
SERGIO TEJEDOR ANTON <sergio.tejedor.anton@l174.lab.dit.upm.es>
2023-11-23 16:56:10 +0100

    a

--- lib_mv.py
+++ lib_mv.py
@@ -106,6 +106,8 @@ class MV:
       configurar_proxy()
       call(["sudo", "virt-copy-in", "-a", self.nombre + ".qcow2", "haproxy.cfg","/etc/haproxy/"])
       call(["rm","haproxy.cfg"])
+
+      call(["sudo", "virt-edit", "-a", self.nombre + ".qcow2", "/etc/rc.local", "-e",  r's/^\s*$/\/usr\/sbin\/haproxyctl restart\n/'])
       # call(["sudo","virt-copy-in", "-a", self.nombre + ".qcow2", "haproxy.cfg", "/etc/haproxy/"])
     #   call(["rm","haproxy.cfg"])
       # call(["sudo","virt-edit", "-a", self.nombre + ".qcow2", "/etc/sysctl.conf", "-e", "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/"])
